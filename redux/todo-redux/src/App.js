70e6b8f3f3ee36fb104ed9dd0328d3d8bc357dc8
Nicholas Luo <nicholasluo@gmail.com>
2017-10-25 23:41:31 +0800

    Refactor App and Header components to be stateless

--- redux/todo-redux/src/App.js
+++ redux/todo-redux/src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import TodoList from "./TodoList";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <TodoList/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <TodoList/>
+  </div>
+);
 
 export default App;
--- redux/todo-redux/src/Header.js
+++ redux/todo-redux/src/Header.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavLink from "./NavLink";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="Header bg-black-90 w-100 ph3 pv3 pv4-ns ph4-m ph5-l tc">
-        <nav className="f6 fw6 ttu tracked">
-          <NavLink to="/">Todo</NavLink>
-        </nav>
-      </header>
-    )
-  }
-}
+const Header = () => (
+  <header className="Header bg-black-90 w-100 ph3 pv3 pv4-ns ph4-m ph5-l tc">
+    <nav className="f6 fw6 ttu tracked">
+      <NavLink to="/">Todo</NavLink>
+    </nav>
+  </header>
+);
 
 export default Header;
